c39599fc194481397ec44a897c6330bd4fefb944
Jobal <jbaldwije@gmail.com>
2020-08-21 16:17:34 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -66,20 +66,20 @@ export default class App extends Component {
             <main className='App__main'>{this.renderMainRoutes()}</main>
           </Context.Provider>
         </div>
-        {/* <footer className="App__footer"> */}
+        <footer className="App__footer">
           {/* <hr /> */}
           {/* <ol className='footInfo'>
             <li><Link className='footLink'>FAQ</Link></li>
             <li><Link className='footLink'>CONTACT US</Link></li>
           </ol> */}
-          {/* <ol className='footIcon'>
+          <ol className='footIcon'>
             <li><a className='fIcon far fa-envelope' href={`mailto:${config.email}`} target='_blank'></a></li>
-            <li><a className='fIcon fab fa-linkedin-in'></a></li> */}
+            <li><a className='fIcon fab fa-linkedin-in'></a></li>
             {/* <li><Link className='fIcon fab fa-facebook-f'></Link></li> */}
-            {/* <li><a className='fIcon fab fa-instagram' href='https://www.instagram.com/sparrowscoffeega/' target='_blank'></a></li>
+            <li><a className='fIcon fab fa-instagram' href='https://www.instagram.com/sparrowscoffeega/' target='_blank'></a></li>
           </ol>
           <p>COPYRIGHT © 2020 SPARROWS COFFEE CO. | ALL RIGHTS RESERVED</p>
-        </footer> */}
+        </footer>
       </div>
     )
   }
